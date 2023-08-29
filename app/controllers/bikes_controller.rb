098d691c3eaf74b15127c86737188b601c1a2f55
Jonathan Mendler <jon.mendler@gmail.com>
2023-08-29 09:59:56 -0500

    Add index action

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,2 +1,6 @@
 class BikesController < ApplicationController
+  def index
+    @bikes = Bike.all
+    render :index
+  end
 end
--- test/controllers/bikes_controller_test.rb
+++ test/controllers/bikes_controller_test.rb
@@ -4,4 +4,12 @@ class BikesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test "index" do
+    get "/bikes.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal Bike.count, data.length
+  end
 end
